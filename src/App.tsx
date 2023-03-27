55ec0752560978f01c6b79e04340122f6fe1196f
TRITIUMNITR0X <tritiumgamingstudios@gmail.com>
2023-03-27 18:57:18 -0400

    + Chrome Extension now working as a popup window.

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width:500, height: 500, backgroundColor:"black", borderRadius: 10}}>
+
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const root = document.createElement("div")
+root.className = "container"
+document.body.appendChild(root)
+const rootDiv = ReactDOM.createRoot(root);
+rootDiv.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
